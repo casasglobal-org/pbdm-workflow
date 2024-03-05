5c9dc9a38994e5d5889ff379405760694bada536
federica.stocchino <federica.stocchino@s2e-pro.com>
2024-03-05 09:39:52 +0100

    update dockerfile and pbdm worker

--- mgd-olive-scripts-main/casas_pbdm_workflow/processing/resources/worker-container/run.sh
+++ mgd-olive-scripts-main/casas_pbdm_workflow/processing/resources/worker-container/run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-
-# cd filesystem/
+cd filesystem/pbdm/
+ls
 mkdir $requestId
-cp -r ../lut/ ./$requestId/lut
-cp ../Olive.ini ../Olive.exe ../pbdm-worker.py ./$requestId/
+cp -r ../../lut/ ./$requestId/lut
+cp ../../Olive.ini ../../Olive.exe ../../pbdm-worker.py ./$requestId/
 cd $requestId/
 mkdir txtfiles && mkdir daily && mkdir output
 #sostituisci con file su efs
-aws s3 cp --recursive s3://dev-pbdm-workflow/$dataset/p/$country/points/ ./txtfiles
-aws s3 cp s3://dev-pbdm-workflow/$dataset/p/$country/coords/punti.dat punti.dat
+#aws s3 cp --recursive s3://dev-pbdm-workflow/$dataset/p/$country/points/ ./txtfiles
+#aws s3 cp s3://dev-pbdm-workflow/$dataset/p/$country/coords/punti.dat punti.dat
 python3 pbdm-worker.py
 sleep 300
 cd ..
